9f919c56b3d74e29c0bf9ae73c18efa11ad48bb8
raza khan <razakhanjmi@gmail.com>
2021-09-23 15:00:05 +0530

    updated ex05

--- ex05/05_transaction.sql
+++ ex05/05_transaction.sql
@@ -1,30 +1,32 @@
 CREATE DATABASE Bank;
 
-
-CREATE TABLE IF NOT EXISTS accounts(
-  account_no INT PRIMARY KEY,
-  id INT NOT NULL,
-  balance INT NOT NULL 
-);
-
 CREATE TABLE IF NOT EXISTS users(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
   email VARCHAR(30) NOT NULL,
-  account_no INT UNIQUE NOT NULL,
-  CONSTRAINT fk_account FOREIGN KEY(account_no) REFERENCES accounts(account_no)
+  account_no INT UNIQUE NOT NULL
 );
 
----- INSERT VALUES
-INSERT INTO accounts 
-VALUES (9900, 1, 100),
-(9911, 2, 0),
-(9922, 3, 600);
 
+CREATE TABLE IF NOT EXISTS accounts(
+  account_no INT UNIQUE NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  balance INT NOT NULL,
+  CONSTRAINT fk_account FOREIGN KEY(id) REFERENCES users(id)
+);
+
+
+---- INSERT VALUES
 INSERT INTO users (name, email, account_no)
-VALUES ('userA', 'userA@email.com', 9900),
-('userB', 'userB@email.com', 9911),
-('userC', 'userC@email.com', 9922);
+VALUES ('userA', 'userA@email.com', 9911),
+('userB', 'userB@email.com', 9922),
+('userC', 'userC@email.com', 9933);
+
+
+INSERT INTO accounts(account_no, balance) 
+VALUES (9911, 100),
+(9922, 0),
+(9933, 600);
 
 --- Before Transaction
 SELECT * FROM accounts;
@@ -32,9 +34,9 @@ SELECT * FROM accounts;
 +------------+----+---------+
 | account_no | id | balance |
 +------------+----+---------+
-|       9900 |  1 |     100 |
-|       9911 |  2 |       0 |
-|       9922 |  3 |     600 |
+|       9911 |  1 |     100 |
+|       9922 |  2 |       0 |
+|       9933 |  3 |     600 |
 +------------+----+---------+
 */
 
@@ -43,9 +45,9 @@ SELECT * FROM users;
 +----+-------+-----------------+------------+
 | id | name  | email           | account_no |
 +----+-------+-----------------+------------+
-|  1 | userA | userA@email.com |       9900 |
-|  2 | userB | userB@email.com |       9911 |
-|  3 | userC | userC@email.com |       9922 |
+|  1 | userA | userA@email.com |       9911 |
+|  2 | userB | userB@email.com |       9922 |
+|  3 | userC | userC@email.com |       9933 |
 +----+-------+-----------------+------------+
 */
 
@@ -54,7 +56,7 @@ SELECT * FROM users;
 START TRANSACTION;
 
 UPDATE accounts AS a INNER JOIN users AS u 
-USING(account_no)
+ON a.account_no = u.account_no
 SET a.balance = a.balance + 1000
 WHERE u.name = 'userA';
 
@@ -65,7 +67,7 @@ COMMIT;
 START TRANSACTION;
 
 UPDATE accounts AS a INNER JOIN users AS u 
-USING(account_no)
+ON a.account_no = u.account_no
 SET a.balance = a.balance - 500
 WHERE u.name = 'userA';
 
@@ -76,12 +78,12 @@ COMMIT;
 START TRANSACTION;
 
 UPDATE accounts AS a INNER JOIN users AS u 
-USING(account_no)
+ON a.account_no = u.account_no
 SET a.balance = a.balance - 200
 WHERE u.name = 'userA';
 
 UPDATE accounts AS a INNER JOIN users AS u 
-USING(account_no)
+ON a.account_no = u.account_no
 SET a.balance = a.balance + 200
 WHERE u.name = 'userB';
 
